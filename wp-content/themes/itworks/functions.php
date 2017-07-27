5adacdae70c27ac45bc43b83d911b4fa050f4679
unknown <mark22312@gmail.com>
2017-07-27 11:05:09 +0300

    added custom files

--- wp-content/themes/itworks/functions.php
+++ wp-content/themes/itworks/functions.php
@@ -25,5 +25,13 @@ register_nav_menu('secondary','secondary header navigation');
 }
 add_action('init','my_theme_setup');
 
+ /* loaders for style and script */
+ function my_theme_script_enque(){
+ 	// custom css
+ 	wp_enqueue_style('CustomStyle', get_template_directory_uri().'/style.css', array(), '1.0.0', 'all');
+ 	// Custom JS
+ 	wp_enqueue_script('customjs', get_template_directory_uri().'/script.js', array('jquery'), '1.0.0',false);
+ 	}
+ add_action('init', 'my_theme_script_enque');
 
 ?>
