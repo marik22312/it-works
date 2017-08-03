4047794689899b248b800bf108999ba5c56f40fb
unknown <mark22312@gmail.com>
2017-08-03 15:23:09 +0300

    set-up WP_Query for section2

--- wp-content/themes/itworks/index.php
+++ wp-content/themes/itworks/index.php
@@ -2,7 +2,30 @@
 get_header();
 ?>
 <section id="index-sectionTwo" class="section fp-auto-height-responsive">
+      <?php
+    // WP_Query arguments
+    $sec2args = array(
+    	'post_type'              => array( 'page' ),
+      'page_name'		         	 => 'about',
+    );
 
+    // The Query
+    $sec2Query = new WP_Query( $sec2args ); ?>
+          <!-- Upcoming games slider  -->
+
+
+
+              <?php if ($sec2Query->have_posts()){?>
+
+                      <?php
+    	 	while ($sec2Query->have_posts()) {
+    		$sec2Query->the_post();
+    		?>
+          <?php the_field('sec2_title'); ?>
+        <?php
+      } }
+    	wp_reset_postdata();
+    	?>
 </section>
 <?php
 get_footer();
