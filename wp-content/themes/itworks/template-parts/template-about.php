b512410ba5d08d1ef8e886cf3e845fe0ebfb5ff3
unknown <mark22312@gmail.com>
2017-08-04 11:11:10 +0300

    working on header

--- wp-content/themes/itworks/template-parts/template-about.php
+++ wp-content/themes/itworks/template-parts/template-about.php
@@ -7,13 +7,45 @@
  * @since It Works 1.0
  */
  ?>
-<?php get_header(); ?>
+<?php get_header();
+
+  if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 <section id="index-sectionTwo" class="section fp-auto-height-responsive">
-<?php
-  if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <div class="about_content">
-      <?php the_content(); ?>
+
+    <div id="sec2_title">
+      <?php the_field('sec2_title'); ?>
+    </div>
+    <div id="sec2_subtitle">
+      <?php the_field('sec2_subtitle'); ?>
+    </div>
+    <div id="about-sec2-dot" class="bluedot"></div>
+    <div id="sec2_con1_title" class="sec2_row_title">
+      <?php the_field('sec2_con1_title'); ?>
+    </div>
+    <div id="sec2_con1_cont" class="sec2_row_cont">
+      <?php the_field('sec2_con1_cont'); ?>
+    </div>
+    <div id="about-sec2-dot" class="bluedot"></div>
+    <div id="sec2_con2_title" class="sec2_row_title">
+      <?php the_field('sec2_con2_title'); ?>
+    </div>
+    <div id="sec2_con2_cont" class="sec2_row_cont">
+      <?php the_field('sec2_con2_cont'); ?>
+    </div>
+    <div id="about-sec2-dot" class="bluedot"></div>
+    <div id="sec2_con3_title" class="sec2_row_title">
+      <?php the_field('sec2_con3_title'); ?>
+    </div>
+    <div id="sec2_con3_cont" class="sec2_row_cont">
+      <?php the_field('sec2_con3_cont'); ?>
+    </div>
+    <div id="about-sec2-dot" class="bluedot"></div>
+    <div id="sec2_con4_title" class="sec2_row_title">
+      <?php the_field('sec2_con4_title'); ?>
+    </div>
+    <div id="sec2_con4_cont" class="sec2_row_cont">
+      <?php the_field('sec2_con4_cont'); ?>
     </div>
   <?php endwhile; else: ?>
     <?php echo "ERROR" ?>
