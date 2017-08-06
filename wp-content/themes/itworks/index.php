ba0922ac8bb67ab676efc5c35946be45bb2a135c
Omri Du-nour <omridunour@gmail.com>
2017-08-06 09:59:16 +0300

    msg here

--- wp-content/themes/itworks/index.php
+++ wp-content/themes/itworks/index.php
@@ -1,7 +1,15 @@
-<?php
-get_header();
-?>
-
-<?php
-get_footer();
-?>
+<?php get_header(); ?>
+	<section class="section" id="section1">
+		<div class="section1-text">
+			
+				<p class="first-line">Advancing disadvantaged young adults<br>
+				to work in the industry</p>
+				<p class="second-line">It works US aims at advancing disadvantaged<br>young adults living in remote areas</p>
+			
+				<center>
+				<button type="button" class="btn btn-info" style="border-radius: 25px;" ><p>Contact us please</button>
+				</center>
+		</div>
+			
+	</section>
+<?php get_footer(); ?>
---
+++ wp-content/themes/itworks/template-parts/template-services.php
@@ -0,0 +1,72 @@
+<?php get_header();
+/**
+ * Template Name: Services
+ *
+ * @package WordPress
+ * @subpackage it_works
+ * @since It Works 1.0
+ */
+get_footer();
+?>
+<section class="row" id="services-section1">
+	<p class="headline">It works US provides top notch services in the areas of:</p>
+		<div class="col-sm-12 col-md-10-offset-1 col-lg-10-offset-1" id="photo">
+			<img src="<?php echo get_template_directory_uri(); ?>/template-parts/bell.png" class="image">
+			<p class="under-photo">Web design and devalopment</p>
+			<p class="paragraph">is simply dummy text of
+			 the printing<br>
+			 and typesetting industry.<br>
+			 Lorem Ipsum has been the industry's</p>
+		</div>
+
+		<div class="col-sm-12 col-md-10-offset-1 col-lg-10-offset-1" id="photo">
+			<img src="<?php echo get_template_directory_uri(); ?>/template-parts/bell.png" class="image">
+			<p class="under-photo">Content management system (CMS)</p>
+			<p class="paragraph">is simply dummy text of
+			 the printing<br>
+			 and typesetting industry.<br>
+			 Lorem Ipsum has been the industry's</p>
+		</div>
+
+		<div class="col-sm-12 col-md-10-offset-1 col-lg-10-offset-1" id="photo">
+			<img src="<?php echo get_template_directory_uri(); ?>/template-parts/bell.png" class="image">
+			<p class="under-photo">Custom application</p>
+			<p class="paragraph">is simply dummy text of
+			 the printing<br>
+			 and typesetting industry.<br>
+			 Lorem Ipsum has been the industry's</p>
+		</div>
+
+		<div class="col-sm-12 col-md-10-offset-1 col-lg-10-offset-1" id="photo">
+			<img src="<?php echo get_template_directory_uri(); ?>/template-parts/bell.png" class="image">
+			<p class="under-photo">Search engine optimization</p>
+			<p class="paragraph">is simply dummy text of
+			 the printing<br>
+			 and typesetting industry.<br>
+			 Lorem Ipsum has been the industry's</p>
+		</div>
+
+		<div class="col-sm-12 col-md-10-offset-1 col-lg-10-offset-1" id="photo">
+			<img src="<?php echo get_template_directory_uri(); ?>/template-parts/bell.png" class="image">
+			<p class="under-photo">e-commerce</p>
+			<p class="paragraph">is simply dummy text of
+			 the printing<br>
+			 and typesetting industry.<br>
+			 Lorem Ipsum has been the industry's</p>
+		</div>
+
+		<div class="col-sm-12 col-md-10-offset-1 col-lg-10-offset-1" id="photo">
+			<img src="<?php echo get_template_directory_uri(); ?>/template-parts/bell.png" class="image">
+			<p class="under-photo">Responsive design</p>
+			<p class="paragraph">is simply dummy text of
+			 the printing<br>
+			 and typesetting industry.<br>
+			 Lorem Ipsum has been the industry's</p>
+		</div>
+</section>
+<section class="row" id="services-section2">
+	<p>Fellows stay to work in their own community<br>
+		with the support and guidence of It Works<br>
+		and its' partners.
+	</p>
+</section>
