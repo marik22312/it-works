802466a013b9be4af3f348cd1baf9ebda08d0373
Omri Du-nour <omridunour@gmail.com>
2017-08-14 08:31:05 +0300

    hi

--- wp-content/themes/itworks/index.php
+++ wp-content/themes/itworks/index.php
@@ -1,15 +1,3 @@
 <?php get_header(); ?>
-	<section class="section" id="section1">
-		<div class="section1-text">
-			
-				<p class="first-line">Advancing disadvantaged young adults<br>
-				to work in the industry</p>
-				<p class="second-line">It works US aims at advancing disadvantaged<br>young adults living in remote areas</p>
-			
-				<center>
-				<button type="button" class="btn btn-info" style="border-radius: 25px;" ><p>Contact us please</button>
-				</center>
-		</div>
-			
-	</section>
+	
 <?php get_footer(); ?>
