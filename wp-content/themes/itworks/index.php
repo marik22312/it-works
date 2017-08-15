b50b2a368f97ab0eb6c0e2cc53b582f4362ab137
Asaaf Hadad <asaf147369@gmail.com>
2017-08-15 14:47:12 +0300

    who knows what we're doing

--- wp-content/themes/itworks/index.php
+++ wp-content/themes/itworks/index.php
@@ -1,13 +1,25 @@
-<<<<<<< HEAD
 <?php
 get_header();
 ?>
 
+<div id="index_section1">
+<span class="index_section1_top_text">it works us offers world-class web services to global customers, leveraging freelance portals and online work</span>
+<span class="index_section1_bottom_text">It Works US aims at advancing disadvantaged young adults living in remote areas</span>
+<button type="button" name="button" class="index_section1_button btn btn-info">CONTACT US >></button>
+</div>
+<div id="index_section2">
+
+</div>
+<div id="index_section3">
+
+</div>
+<div id="index_section4">
+
+</div>
+<div id="index_section5">
+
+</div>
+
 <?php
 get_footer();
 ?>
-=======
-<?php get_header(); ?>
-	
-<?php get_footer(); ?>
->>>>>>> 802466a013b9be4af3f348cd1baf9ebda08d0373
--- wp-content/themes/itworks/template-parts/template-about.php
+++ wp-content/themes/itworks/template-parts/template-about.php
@@ -14,11 +14,9 @@
  ?>
             	<section class="section" id="section1">
  		<div class="section1-text">
-
  				<p class="first-line">Advancing disadvantaged young adults
  				to work in the industry</p>
  				<p class="second-line">It works US aims at advancing disadvantaged<br>young adults living in remote areas</p>
-
  				<center>
  				<button type="button" class="btn btn-info" style="border-radius: 25px;" ><p>Contact us please</button>
  				</center>
