8fdcfd0141f1714ea3eee716bcdf8ce74fe559a1
Omri Du-nour <omridunour@gmail.com>
2017-08-18 11:22:20 +0300

    last change friday 18.8

--- wp-content/themes/itworks/template-parts/template-program.php
+++ wp-content/themes/itworks/template-parts/template-program.php
@@ -9,29 +9,68 @@
 get_footer();
 ?>
 
-<section class="row" id="program-section1">
-	<p class="headline">Our program:</p>
-	<div class="col-sm-10-offset-1 col-md-6 col-lg-6">
-	<p id="section1-subheadline" style="font-size: 36px; margin-left: 10px;">An integrated approach to learning and working in the modern workplace.</p>
+<section class="row" id="program-section1" style="background-color: #f1f1f1;">
+	<p class="headline">Training:</p>
+	<div class="col-sm-10-offset-1 col-md-2-offset-3 col-lg-2-offset-3">
+	<p id="section1-subheadline" style="font-size: 36px; margin-left: 10px; text-align: center;">An integrated approach to learning and working in the modern workplace.</p>
 	</div>
 </section>
-<section class="row" id="program-section2">
-	<p class="section2-subheadline-1" style="font-size: 46px; font-weight: bold; font-style: italic; margin-left: 20px;">Our Bootcamp:</p>
+<section class="row" id="program-section2" style="background-color: #f1f1f1;">
+	<p class="section2-subheadline-1" style="font-size: 46px; font-weight: bold; font-style: italic; margin-left: 20px; text-align: center;">Our Bootcamp:</p>
 
 	<p class="section2-first-paragraph" style="font-size: 24px; margin-left: 20px;">An intensive, hands-on learning program that teaches fellows industry in-demand digital skills, such as:<br>
 	Web Development, Digital Marketing or User Experience (UX) Design.<br>
 	Frontal classes by professional instructors are provided in remote areas in coordination with local municipal offices.</p>
-	<p id="section2-subheadline-2" style="font-size: 36px; margin-left: 20px; font-weight: bold; font-style: italic;">Bootcamps also include:</p>
-	<div class="col-sm-12 col-md-10-offset-1 col-lg-10-offset-1" id="photo">
-		<img src="<?php echo get_template_directory_uri(); ?>/images/english.png" class="image">
-		<p class="under-photo">Technical english course</p>
-		<img src="<?php echo get_template_directory_uri(); ?>/images/job.png" class="image">
-		<p class="under-photo">Job preparation curriculum</p>
-		<img src="<?php echo get_template_directory_uri(); ?>/images/social.png" class="image">
-		<p class="under-photo">Social responsibility and community involvement activities</p>
-		
+	<p id="section2-subheadline-2" style="font-size: 36px; margin-left: 20px; font-weight: bold; font-style: italic; text-align: center;">Bootcamps also include:</p>
+	<div class="col-sm-12 col-md-2-offset-3 col-lg-2-offset-3" id="photo">
+		<div class="under-photo-background" style="background-color: #ae5ee5; height:550px; border-radius: 5px; display: inline-block;">
+		<p class="under-photo" style="color: white; margin-top: 190px;">Technical english course</p>
+		<hr style="width: 300px; border-top: 3px solid white;">
+		<p class="read-more" style="font-style: italic; color: white;">read more</p>
+		</div>
+		<br>
+
+		<div class="under-photo-background" style="background-color: #ae5ee5; height:550px; border-radius: 5px; display: inline-block;">
+		<p class="under-photo" style="color: white; margin-top: 190px;">Social resposibility and community involvement classes</p>
+		<hr style="width: 300px; border-top: 3px solid white;">
+		<p class="read-more" style="font-style: italic; color: white;">read more</p>
+		</div>
+		<br>
+
+		<div class="under-photo-background" style="background-color: #ae5ee5; height:550px; border-radius: 5px; display: inline-block;">
+		<p class="under-photo" style="color: white; margin-top: 190px;">Job preparation curriculum</p>
+		<hr style="width: 300px; border-top: 3px solid white; ">
+		<p class="read-more" style="font-style: italic; color: white;">read more</p>
+		</div>
+		<br>
+
 	</div>
+	<p style="color: #5bc9e4; font-size: 20px; text-align: center; font-style: italic;"> Read More</p>
 </section>
-<section class="row" id="program-section3">
-	
+
+<section class="row" id="program-section3" style="">
+	<p class="headline" style="text-align: center;">GET INVOLVED</p>
+	<div class="col-sm-10-offset-1">
+		<img src="<?php echo get_template_directory_uri(); ?>/images/provide-project.png" class="image" style="margin-bottom: 70px;">
+		<p style="text-align: center;font-size: 18px; font-weight: bold;">Provide us with projects</p>
+	</div>
+	<br>
+	<div class="col-sm-10-offset-1">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/mentor.png" class="image" style="margin-bottom: 70px;">
+		<p style="text-align: center;font-size: 18px; font-weight: bold;">Mentor our fellows</p>
+	</div>
+	<br>
+	<div class="col-sm-10-offset-1">
+		<img src="<?php echo get_template_directory_uri(); ?>/images/hire.png" class="image" style="margin-bottom: 70px;">
+		<p style="text-align: center; font-size: 18px; font-weight: bold;">Hire a graduate</p>
+	</div>
+	<br>
+	<div class="col-sm-10-offset-1">
+		<img src="<?php echo get_template_directory_uri(); ?>/images/donate.png" class="image" style="margin-bottom: 70px;">
+		<p style="text-align: center; font-size: 18px; font-weight: bold;">Donate</p>
+	</div>
+	<br>
+	<center>
+	<button type="button" class="btn btn-info" style=""><p style="font-size: 20px; margin-top: 5px;">Contat us</p></button>
+	</center>
 </section>
