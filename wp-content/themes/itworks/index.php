8a2a9fd5dca835c06dd862abd5bc44c736c21dcb
Asaaf Hadad <asaf147369@gmail.com>
2017-08-21 11:53:35 +0300

    index finished section 1+2

--- wp-content/themes/itworks/index.php
+++ wp-content/themes/itworks/index.php
@@ -5,10 +5,35 @@ get_header();
 <div id="index_section1">
 <span class="index_section1_top_text">it works us offers world-class web services to global customers, leveraging freelance portals and online work</span>
 <span class="index_section1_bottom_text">It Works US aims at advancing disadvantaged young adults living in remote areas</span>
-<button type="button" name="button" class="index_section1_button btn btn-info">CONTACT US >></button>
+<div class="index_section1_button_link"></div>
+<img src="<?php echo get_template_directory_uri(); ?>\images\index\contacUs.png" class="index_section1_button"/>
 </div>
 <div id="index_section2">
-
+<h2 class="index_section2_headline">ABOUT US</h2>
+<div class="row">
+  <div class="index_section2_left_text col-lg-offset-2 col-lg-5">
+    <div class="row">
+<div class="index_section2_top_text">
+  <span class="index_section2_text_number col-lg-1">1/</span>
+  <p class="col-lg-9">
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.
+</p>
+</div>
+</div>
+<div class="row">
+<div class="index_section2_bottom_text">
+  <span class="index_section2_text_number col-lg-1">2/</span>
+  <p class="col-lg-9">
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.
+</p>
+</div>
+  </div>
+  </div>
+  <div class="col-lg-3">
+  <img src="<?php echo get_template_directory_uri(); ?>\images\index\friends.png" alt="friends" class="index_section2_img">
+  </div>
+</div>
+<a class="col-lg-2 col-lg-offset-5 index_section2_bottom">Read More >></a>
 </div>
 <div id="index_section3">
 
