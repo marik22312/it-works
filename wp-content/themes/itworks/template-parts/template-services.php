3b140cd5afda6c9954de17b1ba62ea192b4204aa
Omri Du-nour <omridunour@gmail.com>
2017-08-21 11:55:28 +0300

    monday 12:00

--- wp-content/themes/itworks/template-parts/template-services.php
+++ wp-content/themes/itworks/template-parts/template-services.php
@@ -8,11 +8,11 @@
  */
 get_footer();
 ?>
-<section class="row" id="services-section1">
+<section id="services-section1">
 	<p class="headline" style="margin-bottom: 125px;">How we can help</p>
-
-		<div class="col-xs-12 col-sm-12 col-md-10-offset-1 col-lg-10-offset-1" id="photo">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/web.png" class="image" style="margin-bottom: 80px;">
+	<div class="row">
+		<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4" id="photo">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/web.png" class="image" style="margin-bottom: 80px; margin-top: 30px;">
 			<p class="under-photo">Web design and devalopment</p>
 			<p class="paragraph">is simply dummy text of
 			 the printing<br>
@@ -21,8 +21,8 @@ get_footer();
 		</div>
 		<br>
 
-		<div class="col-xs-12 col-sm-12 col-md-10-offset-1 col-lg-10-offset-1" id="photo">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/content.png" class="image" style="margin-bottom: 80px;">
+		<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4" id="photo">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/content.png" class="image" style="margin-bottom: 80px; margin-top: 20px; max-height: 90px;">
 			<p class="under-photo">Content management system (CMS)</p>
 			<p class="paragraph">is simply dummy text of
 			 the printing<br>
@@ -31,7 +31,7 @@ get_footer();
 		</div>
 
 		<br>
-		<div class="col-xs-12 col-sm-12 col-md-10-offset-1 col-lg-10-offset-1" id="photo">
+		<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4" id="photo">
 			<img src="<?php echo get_template_directory_uri(); ?>/images/custom.png" class="image" style="margin-bottom: 80px;">
 			<p class="under-photo">Custom application</p>
 			<p class="paragraph">is simply dummy text of
@@ -40,9 +40,11 @@ get_footer();
 			 Lorem Ipsum has been the industry's</p>
 		</div>
 		<br>
+	</div>	
 
-		<div class="col-xs-12 col-sm-12 col-md-10-offset-1 col-lg-10-offset-1" id="photo">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/search.png" class="image" style="margin-bottom: 80px;">
+	<div class="row">
+		<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4" id="photo">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/search.png" class="image" style="margin-bottom: 80px; margin-top: 40px;">
 			<p class="under-photo">Search engine optimization</p>
 			<p class="paragraph">is simply dummy text of
 			 the printing<br>
@@ -51,8 +53,8 @@ get_footer();
 		</div>
 		<br>
 
-		<div class="col-xs-12 col-sm-12 col-md-10-offset-1 col-lg-10-offset-1" id="photo">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/e-commerce.png" class="image" style="margin-bottom: 80px;">
+		<div class="col-xs-12 col-sm-12 col-md-2 col-lg-4" id="photo">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/e-commerce.png" class="image" style="margin-bottom: 80px; margin-top: 80px;">
 			<p class="under-photo">e-commerce</p>
 			<p class="paragraph">is simply dummy text of
 			 the printing<br>
@@ -61,14 +63,15 @@ get_footer();
 		</div>
 		<br>
 
-		<div class="col-xs-12 col-sm-12 col-md-10-offset-1 col-lg-10-offset-1" id="photo">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/responsive.png" class="image" style="margin-bottom: 80px;">
+		<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4" id="photo">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/responsive.png" class="image" style="margin-bottom: 80px; margin-top: 50px;">
 			<p class="under-photo">Responsive design</p>
 			<p class="paragraph">is simply dummy text of
 			 the printing<br>
 			 and typesetting industry.<br>
 			 Lorem Ipsum has been the industry's</p>
 		</div>
+	</div>
 	</section>
 
 		<p style="color: #5bc9e4; font-size: 20px; text-align: center; font-style: italic;"> Read More</p>
