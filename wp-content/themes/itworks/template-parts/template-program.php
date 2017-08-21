2ca34e9ac16435525ecf3455a77113108d903664
Omri Du-nour <omridunour@gmail.com>
2017-08-21 14:14:07 +0300

    changes monday 14:15

--- wp-content/themes/itworks/template-parts/template-program.php
+++ wp-content/themes/itworks/template-parts/template-program.php
@@ -10,73 +10,73 @@ get_footer();
 ?>
 
 <section id="program-section1" style="background-color: #f1f1f1;">
-	<p class="headline">Training</p>
+	<p class="headline" style="margin-bottom: 100px;">Training</p>
 
 	<center>
 	<p class="section2-subheadline-1" style="font-size: 46px; font-weight: bold; font-style: italic; margin-left: 20px; text-align: center;">Our Bootcamp</p>
 
-	<p class="section2-first-paragraph" style="font-size: 24px; margin-left: 20px;">An intensive, hands-on learning program that teaches fellows industry in-demand digital skills, such as:<br>
+	<p class="section2-first-paragraph" style="font-size: 24px; margin-left: 20px; margin-bottom: 100px;">An intensive, hands-on learning program that teaches fellows industry in-demand digital skills, such as:<br>
 	Web Development, Digital Marketing or User Experience (UX) Design.<br>
 	Frontal classes by professional instructors are provided in remote areas in coordination with local municipal offices.</p>
-	<p id="section2-subheadline-2" style="font-size: 36px; margin-left: 20px; font-weight: bold; font-style: italic; text-align: center;">Bootcamps also include</p>
+	<p id="section2-subheadline-2" style="font-size: 36px; margin-left: 20px; margin-bottom: 100px; font-weight: bold; font-style: italic; text-align: center;">Bootcamps also include</p>
 	</center>
 
 <div class="row">
-		<div class="under-photo-background col-xs-12 col-sm-12 col-md-4 col-lg-2 col-lg-offset-2" id="photo" style=" background-color: #ae5ee5; height:550px; border-radius: 5px; margin-top: 40px;">
+		<div class="under-photo-background col-xs-12 col-sm-12 col-md-4 col-lg-2 col-lg-offset-2" id="photo" style=" background-color: #ae5ee5; height:550px; border-radius: 10px; margin-top: 40px;">
 		<p class="under-photo" style="color: white; margin-top: 190px;">Technical english course</p>
 		<hr style="width: 200px; border-top: 3px solid white;">
-		<p class="read-more" style="font-style: italic; color: white;">read more</p>
+		<p class="read-more" style="font-style: italic; color: white; font-size: 18px">read more >></p>
 		</div>
 		<br>
 
-		<div class="under-photo-background col-xs-12 col-sm-12 col-md-4 col-lg-2 col-lg-offset-1" id="photo" style="background-color: #ae5ee5; height:550px;  border-radius: 5px; margin-top: 20px;">
+		<div class="under-photo-background col-xs-12 col-sm-12 col-md-4 col-lg-2 col-lg-offset-1" id="photo" style="background-color: #ae5ee5; height:550px;  border-radius: 10px; margin-top: 20px;">
 		<p class="under-photo" style="color: white; margin-top: 190px;">Social resposibility and community involvement classes</p>
-		<hr style="width: 200px; border-top: 3px solid white;">
-		<p class="read-more" style="font-style: italic; color: white;">read more</p>
+		<hr style="width: 180px; border-top: 3px solid white;">
+		<p class="read-more" style="font-style: italic; color: white; font-size: 18px;">read more >></p>
 		</div>
 		<br>
 
 
-		<div class="under-photo-background col-xs-12 col-sm-12 col-md-4 col-lg-2 col-lg-offset-1" style="background-color: #ae5ee5; height:550px; border-radius: 5px;">
+		<div class="under-photo-background col-xs-12 col-sm-12 col-md-4 col-lg-2 col-lg-offset-1" style="background-color: #ae5ee5; height:550px; border-radius: 10px;">
 		<p class="under-photo" style="color: white; margin-top: 190px; text-align: center;">Job preparation curriculum</p>
 		<hr style="width: 200px; border-top: 3px solid white; ">
-		<p class="read-more" style="font-style: italic; color: white; text-align: center;">read more</p>
+		<p class="read-more" style="font-style: italic; color: white; text-align: center; font-size: 18px">read more >></p>
 		</div>
 		<br>
 	
 
 </div>
 
-	<p style="color: #5bc9e4; font-size: 20px; text-align: center; font-style: italic;"> Read More</p>
+	<p style="color: #5bc9e4; font-size: 20px; text-align: center; font-style: italic; margin-top: 135px;"> Read More >></p>
 </section>
 
 <section id="program-section3" style="">
-	<p class="headline" style="text-align: center;">GET INVOLVED</p>
+	<p class="headline" style="text-align: center; margin-top: 145px; margin-bottom: 190px;">GET INVOLVED</p>
 
 <div class="row">
-	<div class="col-xs-12 col-sm-12 col-md-2 col-md-offset-2 col-lg-3 col-lg-offset-1">
+	<div class="col-xs-12 col-sm-12 col-md-2 col-md-offset-2 col-lg-2 col-lg-offset-1" style="margin-top: 60px;">
 		<img src="<?php echo get_template_directory_uri(); ?>/images/provide-project.png" class="image" style="margin-bottom: 70px;">
 		<p style="text-align: center;font-size: 18px; font-weight: bold;">Provide us with projects</p>
 	</div>
 	<br>
-	<div class="col-xs-12 col-sm-12 col-md-2 col-md-offset-2 col-lg-3 col-lg-offset-1">
+	<div class="col-xs-12 col-sm-12 col-md-2 col-md-offset-2 col-lg-2 col-lg-offset-1" style="margin-top: 50px;">
 			<img src="<?php echo get_template_directory_uri(); ?>/images/mentor.png" class="image" style="margin-bottom: 70px;">
 		<p style="text-align: center;font-size: 18px; font-weight: bold;">Mentor our fellows</p>
 	</div>
 	<br>
-	<div class="col-xs-12 col-sm-12 col-md-2 col-md-offset-2 col-lg-3 col-lg-offset-1">
+	<div class="col-xs-12 col-sm-12 col-md-2 col-md-offset-2 col-lg-2 col-lg-offset-1" style="margin-top: 20px;">
 		<img src="<?php echo get_template_directory_uri(); ?>/images/hire.png" class="image" style="margin-bottom: 70px;">
 		<p style="text-align: center; font-size: 18px; font-weight: bold;">Hire a graduate</p>
 	</div>
 	<br>
-	<div class="col-xs-12 col-sm-12 col-md-2 col-md-offset-2 col-lg-3 col-lg-offset-1">
-		<img src="<?php echo get_template_directory_uri(); ?>/images/donate.png" class="image" style="margin-bottom: 70px;">
-	</div>
-		<p style="text-align: center; font-size: 18px; font-weight: bold;">Donate</p>
+	<div class="col-xs-12 col-sm-12 col-md-2 col-md-offset-2 col-lg-2 col-lg-offset-1" style="margin-bottom: 10px;">
+		<img src="<?php echo get_template_directory_uri(); ?>/images/donate.png" class="image" style="margin-bottom: 70px; margin-right: 100px; height: 100px;">
 	
+		<p style="text-align: center; font-size: 18px; font-weight: bold;">Donate</p>
+	</div>
 	<br>
 </div>
 	<center>
-	<button type="button" class="btn btn-info" style=""><p style="font-size: 20px; margin-top: 5px;">Contat us</p></button>
+	<button type="button" class="btn btn-info" style="margin-top: 195px; height: 60px; border-radius: 20px;"><p style="font-size: 20px;">Contact us</p></button>
 	</center>
 </section>
